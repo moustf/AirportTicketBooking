d6e1ed7d440921464c11e38a34b7f07723d64e23
Mustafa Salem <mustafaissasalem@gmail.com>
2023-08-17 10:41:27 +0300

    feat: add .BL suffix to the business layer project

--- AirportTicketBooking.BL/Properties/AssemblyInfo.cs
+++ AirportTicketBooking.BL/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AirportTicketBooking")]
+[assembly: AssemblyTitle("AirportTicketBooking.BL")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AirportTicketBooking")]
+[assembly: AssemblyProduct("AirportTicketBooking.BL")]
 [assembly: AssemblyCopyright("Copyright ©  2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
